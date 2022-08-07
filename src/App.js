ac678d122bc487b4f00e605412d7cfd1808eef12
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-08-08 01:00:22 +0600

    reload problem solve

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [isReload, setISReload] = useState(false);
 
   useEffect(() => {
 
@@ -26,7 +27,7 @@ to handle search by query, and it will be passed as props to header
 
     event.preventDefault();
     const queryText = event.target.searchText.value;
-    
+
     if(queryText){      
     fetch(`http://localhost:5000/notes?userName=${queryText}`)
     .then((res)   => res.json())
@@ -74,12 +75,37 @@ to delete a note, and it will be passed as props to NoteCard that will be trigge
 to post data to backend, and it will be passed as props to InputFrom.
  */
 
+const handlePost =(event) =>{
+  event.preventDefault();
+
+  const userName = event.target.userName.value;
+  const textData = event.target.textData.value;
+
+  console.log({userName, textData});
+
+  fetch("http://localhost:5000/note",{
+    method:"POST",
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+
+    body:JSON.stringify({userName, textData}),
+  }).then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    setISReload(!isReload);  
+  });
+};
+
+
+
+
   
 
   return (
     <div className="App">
       <Header  handleSearch={handleSearch}/>
-      <InputForm />
+      <InputForm handlePost={handlePost} />
       <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
         {notes.map((note) => (
           <NoteCard
